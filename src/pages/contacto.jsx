6a5dc476b3743e1ca29e5f289ffbd19710a3fb24
Nacho Trevisan <nachotizii988@gmail.com>
2024-08-31 19:10:26 -0300

    add portfolio

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -1,30 +1,106 @@
+import { Button, Grid, Paper, styled, TextField } from '@mui/material';
 import style from '../css/contacto.module.css'
 import { FaLinkedin, FaEnvelope, FaGithub, FaInstagram } from 'react-icons/fa';
+import { UseForm } from '../hooks/useForm';
+import { collection, doc, setDoc } from 'firebase/firestore';
+import { FireBaseDB } from '../firebase/config';
+import 'react-toastify/dist/ReactToastify.css';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+
+
+const formData = {
+    sugerencia: ''
+}
 
 export const Contacto = () => {
+    const { sugerencia, OnInputchange } = UseForm(formData);
+    const submit = async () => {
+        if (sugerencia.length < 1) return;
+        event.preventDefault();
+        const body = {
+            mensaje: sugerencia
+        }
+        const newDoc = doc(collection(FireBaseDB, `sugerencias/`))
+        await setDoc(newDoc, body);
+        toast.success(' Muchisimas gracias', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce
+        });
+    }
+
     return (
-        <div className={style.container}>
+        <>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition:Bounce
+            />
+
+            <br></br>
+            <form onSubmit={submit}>
+                <Grid container mb={'10'} >
 
 
-            <h1>¿Tienes algún proyecto en mente?</h1>
-            <p>Contactame y trabajemos juntos!</p>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'center', gap: '5%', marginTop: '2%' }}>
+                    <Grid item xs={11}>
+                        <h1>Sugerencias...</h1>
+                        <TextField
+                            id="outlined-multiline-flexible"
+                            label="Sugerencias sobre el porfolio (Se envia a Firebase, no es necesario autenticación)"
+                            multiline
+                            rows={2}
+                            fullWidth
+                            name='sugerencia'
+                            value={sugerencia}
+                            onChange={OnInputchange}
+                        />
+                    </Grid>
+                    <Grid container xs={11} justifyContent='flex-end' pt={'20px'}>
+                        <Button variant="contained" type='submit'>Enviar</Button>
+                    </Grid>
+
+                </Grid>
+            </form>
+
+            <div className={style.container}>
 
 
-                <a href="https://github.com/ignacioTrevisan" className={style.icon_link}><div ><FaGithub /></div></a>
-
-                <a href="mailto:nachotizii988@gmail.com" className={style.icon_link}><FaEnvelope /></a>
-
-                <a href="https://www.linkedin.com/in/ignacio-trevisan-799992240/" className={style.icon_link}><FaLinkedin /></a>
-
-                <a href="https://www.instagram.com/nacho.trevisan/" className={style.icon_link}>
-                    <FaInstagram />
-                </a>
-
-            </div>
+                <h1>¿Tienes algún proyecto en mente?</h1>
+                <p>¡Contactame y trabajemos juntos!</p>
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'center', gap: '5%' }}>
 
 
-        </div >
+                    <a href="https://github.com/ignacioTrevisan" className={style.icon_link}><div ><FaGithub /></div></a>
+
+                    <a href="mailto:nachotizii988@gmail.com" className={style.icon_link}><FaEnvelope /></a>
+
+                    <a href="https://www.linkedin.com/in/ignacio-trevisan-799992240/" className={style.icon_link}><FaLinkedin /></a>
+
+                    <a href="https://www.instagram.com/nacho.trevisan/" className={style.icon_link}>
+                        <FaInstagram />
+                    </a>
+
+
+                </div>
+                <br></br>
+
+
+            </div >
+        </>
     );
 }
 
--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -9,7 +9,10 @@ export const Inicio = () => {
             </div>
             <div className={styles.textoContainer}>
                 <h1> <span style={{ color: '#1E90FF' }}>¡Hola!</span> Me llamo Ignacio Trevisan</h1>
-                <p> Tengo 22 años y vivo en Entre Ríos, Argentina. Este es mi portafolio personal, donde podrás encontrar información sobre mis estudios, proyectos, más detalles sobre mí y mis medios de contacto. Espero te sea utíl</p>
+                <p> Tengo 22 años y vivo en Entre Ríos, Argentina. Este es mi portafolio personal, donde podrás encontrar información sobre mis estudios, proyectos, más detalles sobre mí y mis medios de contacto. Espero te sea utíl
+                    <p>También dejo adjunto mi <a href='../../public/CvProg.pdf' download='CvProg.pdf'> Curriculum vitae </a> por si por alguna razón, llegaste a este punto sin que te lo haya pasado de ante mano. </p>
+
+                </p>
             </div>
         </div>
     )
