3a42fcf7d90635af0479223bc698305a7215b1ad
Nacho Trevisan <nachotizii988@gmail.com>
2024-12-12 19:06:40 -0300

    cv cambiado

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -11,7 +11,7 @@ export const Inicio = () => {
             <div style={{ maxWidth: screenWidth > 600 ? '60%' : '100%' }}>
                 <h1> <span style={{ color: '#1E90FF' }}>¡Hola!</span> Me llamo Ignacio Trevisan</h1>
                 <p> Tengo 22 años y vivo en Entre Ríos, Argentina. Este es mi porfolio personal, donde podrás encontrar información sobre mis estudios, proyectos, más detalles sobre mí y mis medios de contacto. Espero te sea utíl
-                    <p>También dejo adjunto mi <a href='https://drive.google.com/file/d/1-keX-V_7884QFnCbHPA3vGT62h-UMBk_/view?usp=sharing' download='CvProg.pdf'> Curriculum vitae </a> por si por alguna razón, llegaste a este punto sin que te lo haya pasado de ante mano. </p>
+                    <p>También dejo adjunto mi <a href='https://drive.google.com/file/d/1MTXH4BX2S5T_6fJAtUgNoeg1alEdus1R/view?usp=sharing' download='CvProg.pdf'> Curriculum vitae </a> por si por alguna razón, llegaste a este punto sin que te lo haya pasado de ante mano. </p>
 
                 </p>
             </div>
